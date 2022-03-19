fc59d75f2eba9e9a01a83714ad3068b30f1f6aab
nicoleribeiro <nicole.ribeiro1612@gmail.com>
2022-03-19 20:57:09 -0300

    diretiva ngFor

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  // exemplo de objeto em typescript
+  // any pq nao tem tipo definido
+  public eventos: any = [
+    {
+      Tema: 'Angular',
+      Local: 'São Paulo'
+    },
+    {
+      Tema: '.NET',
+      Local: 'Rio de Janeiro'
+    },
+    {
+      Tema: 'Angular e suas novidades',
+      Local: 'Belo Horizonte'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
