3128f0fc235d1e7643a278799be55e39831db9f2
nicoleribeiro <nicole.ribeiro1612@gmail.com>
2022-03-19 21:23:18 -0300

    metodo get buscando dados da api

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,24 +10,52 @@ export class EventosComponent implements OnInit {
 
   // exemplo de objeto em typescript
   // any pq nao tem tipo definido
-  public eventos: any = [
-    {
-      Tema: 'Angular',
-      Local: 'São Paulo'
-    },
-    {
-      Tema: '.NET',
-      Local: 'Rio de Janeiro'
-    },
-    {
-      Tema: 'Angular e suas novidades',
-      Local: 'Belo Horizonte'
-    }
-  ]
+  // public eventos: any = [
+  //   {
+  //     Tema: 'Angular',
+  //     Local: 'São Paulo'
+  //   },
+  //   {
+  //     Tema: '.NET',
+  //     Local: 'Rio de Janeiro'
+  //   },
+  //   {
+  //     Tema: 'Angular e suas novidades',
+  //     Local: 'Belo Horizonte'
+  //   }
+  // ]
 
-  constructor() { }
+  public eventos: any;
 
+  constructor(private http: HttpClient) { }
+
+  // ngOnInit chama ao iniciar a aplicação
   ngOnInit(): void {
+    this.getEventos();
   }
 
+  // método get
+  public getEventos():void{
+    // objeto com dados vindos da api
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+    // objeto com dados estáticos
+    // this.eventos = [
+    //   {
+    //     Tema: 'Angular',
+    //     Local: 'São Paulo'
+    //   },
+    //   {
+    //     Tema: '.NET',
+    //     Local: 'Rio de Janeiro'
+    //   },
+    //   {
+    //     Tema: 'Angular e suas novidades',
+    //     Local: 'Belo Horizonte'
+    //   }
+    // ]
+  }
 }
